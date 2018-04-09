8f8ba0c2b666065158aaee074ef6fa3f7215d65e
王建斌 <wangjianbin@douBeedemacbookair.local>
2018-04-09 18:06:00 +0800

    测试添加文件

---
+++ test_BDPhotoPicker/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  test_BDPhotoPicker
+//
+//  Created by 王建斌 on 2018/4/9.
+//  Copyright © 2018年 buydeem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
